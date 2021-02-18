bfd09a5e91bbe9fa804f0229207177243c083563
Akshay Mishra <42304248+Jontyabhay@users.noreply.github.com>
2021-02-19 04:50:26 +0530

    Delete Code_Intel.py

--- Code_Intel.py
+++
@@ -1,9 +0,0 @@
-def calc():
-    a = input("Enter a number ")
-    c = 0
-    for i in range(0, int(a) ** 2):
-        print("*", end=" ")
-        c = c + 1
-        if (c % int(a) == 0):
-            print(end="\n")
-calc()
