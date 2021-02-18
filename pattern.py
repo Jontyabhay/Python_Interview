d9a9a32d33b58fc7e75f85300c66a0594dbe1b1a
Akshay Mishra <42304248+Jontyabhay@users.noreply.github.com>
2021-02-19 04:50:53 +0530

    new code

--- pattern.py
+++ pattern.py
@@ -3,6 +3,16 @@ def pattern():
     for i in range(int(a)):
         for j in range(i+1):
             print("*", end =" ")
-        print(end='\n')
+        print(end='\n')   
 
+def another_pattern():
+    a = 0
+    inp = input("Enter a number ")
+    for i in range(int(inp) ** 2):
+        print("*", end=" ")
+        a = a + 1
+        if (a % int(inp) == 0):
+            print(end='\n')
+
+another_pattern()
 pattern()            
